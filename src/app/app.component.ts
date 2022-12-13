f98fa17e9b0760fd5758c12200a3dfb02e31fb07
Erin Clark <erincodepirate@gmail.com>
2022-12-12 17:48:05 -0800

    Add httpClient, services and call in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angularhttp';
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.onGetUsers();
+    this.onGetUser();
+  }
+
+  onGetUsers(): void {
+    this.userService.getUsers().subscribe(
+      {
+        next: (res) => {
+          console.log(res);
+        },
+        error: (err) => {
+          console.log(err);
+        },
+        complete: () => {
+          console.log("Workers of the world unite!");
+        }
+      })
+  }
+
+  onGetUser(): void {
+    this.userService.getUser().subscribe(
+      {
+        next: (res) => {
+          console.log(res);
+        },
+        error: (err) => {
+          console.log(err);
+        },
+        complete: () => {
+          console.log("Workers of the world unite!");
+        }
+      })
+  }
 }
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../interfaces/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  url = 'http://jsonplaceholder.typicode.com/users/'
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.url);
+  }
+
+  getUser(userid = 1): Observable<User> {
+    return this.http.get<User>(this.url + userid);
+  }
+}
